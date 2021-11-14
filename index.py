a9170fe0bf815debbaa87e33cfa665dcc2e982c2
refael test <refael058325@gmail.com>
2021-11-14 14:01:49 +0000

    update html file

--- index.py
+++ index.py
@@ -17,43 +17,22 @@ def test(name):
 
 @app.route('/' , methods=['GET','POST'])
 def home():
-       return render_template('home.html')
+    return render_template('home.html')
 
-Namespace='trutyru'
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-        
-    # namespace = request.form['namespace']
-    # chart = request.form['chart']
-    # host = request.form['host']
-    # repo = request.form['repo']
-    # tag = request.form['tag']
-    # reg = request.form['reg']
-    # branch = request.form['branch']
-    # homedir = request.form['homedir']
+    namespace = request.form['namespace']
+    chart = request.form['chart']
+    host = request.form['host']
+    repo = request.form['repo']
+    tag = request.form['tag']
+    reg = request.form['reg']
+    branch = request.form['branch']
+    homedir = request.form['homedir']
 
-    # Namespace = namespace.upper()
-    # Chart = chart.upper()
-    # Host = host.upper()
-    # Repo = repo.upper()
-    # Tag = tag.upper()
-    # Reg = reg.upper()
-    # Branch = branch.upper()
-    # Homedir = homedir.upper()
 
-    test(Namespace)
+    test(namespace)
     return render_template('log.html')
 
     
-
-
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-    
-#     name = 'default_name'
-#     # if request.method == 'POST' :
-#     name = request.form['username']
-#     os.system('mkdir '+name)
-#     # else:
-
-
